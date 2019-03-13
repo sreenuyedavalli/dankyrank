23dd64f61b6aea77ca0f404166f3b9d4b0b3b3f3
Sreenu Yedavalli <sreenu.yedavalli@braze.com>
2019-03-13 15:26:58 -0400

    updated ondelete on foriegnkey

--- app/home/models.py
+++ app/home/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Song(models.Model):
+    artist = models.CharField(max_length=100)
+    spotify_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
 
 class Playlist(models.Model):
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     spotify_playlist_id = models.CharField(max_length=255)
 
 class Group(models.Model):
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    master_playlist = models.ForeignKey(Playlist)
+    master_playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
 
 class Membership(models.Model):
-    group = models.ForeignKey(Group)
-    user = models.ForeignKey(User)
-    playlist = models.ForeignKey(Playlist)
-
-class Songs(models.Model):
-    artist = models.CharField(max_length=100)
-    spotify_id = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
